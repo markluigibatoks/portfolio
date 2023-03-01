c44af643eb0cfe55482a64e229b6d1260a144455
Mark-Luigi Batoctoy <markluigibatoks@gmail.com>
2023-03-01 10:54:01 +0800

    task: add TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <base-section class="min-h-[60px]">
+        <base-logo>
+          <img src="" alt=""/>
+        </base-logo>
+
+        <!-- <div class="head_info">
+          <h2>Contact <mark>number</mark></h2>
+        </div> -->
+
+        <base-list
+          :items="Array.from({length: 2}, (_, i) => i)"
+        >
+          <template #item>
+            <base-logo>
+              <img src="" alt=""/>
+            </base-logo>
+          </template>
+        </base-list>
+    </base-section>
+  </header>
+</template>
