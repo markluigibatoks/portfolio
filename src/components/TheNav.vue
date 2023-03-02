71206a73458f9f2c84c3007eb7cd91964a697413
Mark-Luigi Batoctoy <markluigibatoks@gmail.com>
2023-03-02 15:53:41 +0800

    chore: improve TheNav

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -12,42 +12,54 @@ const menu = [
 const open = ref(false)
 const target = ref('')
 
-const targetHeight = computed(() => {
-  return target.value.clientHeight + 20
-})
+const targetHeight = ref('0px')
 
+useResizeObserver(target, ([entry]) => {
+  targetHeight.value = entry.borderBoxSize.reduce((acc, { blockSize }) => acc + blockSize, 0) + 'px'
+})
 </script>
 
 <template>
-<base-section>
-  <div 
-    ref="target" 
-    class="z-20 transition-all ease-in-out"
+  <base-container
+    class="z-20 relative bg-white"
     :class="[{ 'shadow shadow-lg': open }]"
   >
-    <base-logo>
-      <img src="" alt="" />
-    </base-logo>
-
-    <base-hamburger
-      :open="open"
-      @click:hamburger="open = !open"
-    />
-  </div>
-
-  <base-wrapper
-    class="overflow-hidden z-10 dynamic-padding fixed top-0 w-screen bg-white transition-all ease-in-out" 
-    :class="[{ open ? 'h-screen': 'h-0' }]"
-  >
-    <base-list
-      :items="menu"
+    <div
+      ref="target" 
+      class="relative flex justify-between items-center transition-all ease-in-out"
     >
-      <template #item>
-        Test
-      </template>
-    </base-list>
-  </base-wrapper>
-</base-section>
+      <base-logo class="float-left">
+        <img src="../assets/logo.svg" alt="logo" class="w-16 h-16"/>
+      </base-logo>
+
+      <base-hamburger
+        class="lg:hidden block"
+        :open="open"
+        @click:hamburger="open = !open"
+      />
+      <base-list
+        class="lg:flex hidden"
+        :items="menu"
+        main-axis="horizontal"
+        >
+          <template #item>
+            Test
+          </template>
+      </base-list>
+    </div>
+  </base-container>
+  <base-container
+      class="lg:hidden block overflow-hidden z-10 fixed top-0 left-0 w-screen bg-red-100 transition-all ease-in-out" 
+      :class="[ open ? 'h-screen dynamic-padding': 'h-0 pt-0' ]"
+    >
+      <base-list
+        :items="menu"
+      >
+        <template #item>
+          Test
+        </template>
+      </base-list>
+  </base-container>
 </template>
 
 <style scoped>
