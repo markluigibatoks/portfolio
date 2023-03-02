246e5523dda8bf9c75d6d58ecbd2640bef63a468
Mark-Luigi Batoctoy <markluigibatoks@gmail.com>
2023-03-02 15:51:50 +0800

    chore: implement container

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,23 +1,31 @@
 <template>
   <header>
-    <base-section class="min-h-[60px]">
-        <base-logo>
-          <img src="" alt=""/>
-        </base-logo>
+    <base-container class="min-h-[60px]">
+      <base-logo>
+        <img
+          src="../assets/logo.svg"
+          alt="logo"
+          class="w-14 h-14"
+        >
+      </base-logo>
 
-        <!-- <div class="head_info">
+      <!-- <div class="head_info">
           <h2>Contact <mark>number</mark></h2>
         </div> -->
 
-        <base-list
-          :items="Array.from({length: 2}, (_, i) => i)"
-        >
-          <template #item>
-            <base-logo>
-              <img src="" alt=""/>
-            </base-logo>
-          </template>
-        </base-list>
-    </base-section>
+      <base-list
+        :items="Array.from({length: 2}, (_, i) => i)"
+        main-axis="horizontal"
+      >
+        <template #item>
+          <base-logo>
+            <img
+              src="../assets/logo.svg"
+              alt="logo"
+            >
+          </base-logo>
+        </template>
+      </base-list>
+    </base-container>
   </header>
 </template>
