c575763a364f3a7877b34102cfd6bf0a02c68c8a
MRKL\63915 <markluigibatoks@gmail.com>
2023-03-19 00:15:43 +0800

    task: fix spacingClass and add additional gap values

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -39,7 +39,7 @@ const props = defineProps({
     type: Number,
     default: 0,
     validator (value) {
-      const gapValues = ['gap-x-0', 'gap-x-0.5', 'gap-x-1', 'gap-x-2', 'gap-x-3', 'gap-x-4', 'gap-x-5', 'gap-x-6', 'gap-x-7', 'gap-x-8', 'gap-x-9', 'gap-x-10']
+      const gapValues = ['gap-x-0', 'gap-x-0.5', 'gap-x-1', 'gap-x-2', 'gap-x-3', 'gap-x-4', 'gap-x-5', 'gap-x-6', 'gap-x-7', 'gap-x-8', 'gap-x-9', 'gap-x-10', 'gap-x-11', 'gap-x-12']
       return gapValues.includes(`gap-x-${value}`)
     }
   },
@@ -47,7 +47,7 @@ const props = defineProps({
     type: Number,
     default: 0,
     validator (value) {
-      const gapValues = ['gap-y-0', 'gap-y-0.5', 'gap-y-1', 'gap-y-2', 'gap-y-3', 'gap-y-4', 'gap-y-5', 'gap-y-6', 'gap-y-7', 'gap-y-8', 'gap-y-9', 'gap-y-10']
+      const gapValues = ['gap-y-0', 'gap-y-0.5', 'gap-y-1', 'gap-y-2', 'gap-y-3', 'gap-y-4', 'gap-y-5', 'gap-y-6', 'gap-y-7', 'gap-y-8', 'gap-y-9', 'gap-y-10', 'gap-x-11', 'gap-x-12']
       return gapValues.includes(`gap-y-${value}`)
     }
   },
@@ -82,7 +82,11 @@ const emit = defineEmits(['handle:intersecting'])
 const spacingClass = computed(() => {
   const list = []
 
-  list.push(`gap-x-${props.mainAxisSpacing}`, `gap-y-${props.crossAxisSpacing}`)
+  if (props.mainAxis === 'horizontal') {
+    list.push(`gap-x-${props.mainAxisSpacing}`, `gap-y-${props.crossAxisSpacing}`)
+  } else {
+    list.push(`gap-x-${props.crossAxisSpacing}`, `gap-y-${props.mainAxisSpacing}`)
+  }
 
   return list
 })
