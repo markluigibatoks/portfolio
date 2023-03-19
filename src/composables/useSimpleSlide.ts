3bcc1a3a6926d2d4cafbb0b1d37a85b48cd3ed5e
MRKL\63915 <markluigibatoks@gmail.com>
2023-03-19 20:06:31 +0800

    chore: add useSimpleSlide composable

---
+++ src/composables/useSimpleSlide.ts
@@ -0,0 +1,48 @@
+const slideIndex = ref(0)
+const slideInterval = ref()
+const slideLength = ref()
+
+function startInterval () {
+  slideInterval.value = setInterval(() => {
+    handleNext()
+  }, 3000)
+}
+
+function stopInterval () {
+  clearInterval(slideInterval.value)
+}
+
+function handleNext () {
+  if (slideIndex.value + 1 >= slideLength.value) {
+    slideIndex.value = 0
+  } else {
+    slideIndex.value += 1
+  }
+  stopInterval()
+  startInterval()
+}
+
+function handlePrevious () {
+  if (slideIndex.value - 1 < 0) {
+    slideIndex.value = slideLength.value - 1
+  } else {
+    slideIndex.value -= 1
+  }
+  stopInterval()
+  startInterval()
+}
+
+export default (length = 0) => {
+
+  slideLength.value = length
+  startInterval()
+
+  return {
+    slideIndex,
+    slideInterval,
+    startInterval,
+    stopInterval,
+    handleNext,
+    handlePrevious
+  }
+}
