46fdd249d61c42c42d398a6417c589de68741c19
MRKL\63915 <markluigibatoks@gmail.com>
2023-03-19 20:44:18 +0800

    chore: implement simpleSlide

--- src/components/TheBottom2.vue
+++ src/components/TheBottom2.vue
@@ -1,3 +1,54 @@
+<script setup>
+const testimonials = ref([
+  {
+    name: 'Alex',
+    img: '/images/avatar-1.png',
+    content: '"Excellent communication and service - transformed our website!"',
+    job: 'Small Business Owner'
+  },
+  {
+    name: 'Sarah',
+    img: '/images/avatar-3.png',
+    content: '"Professional, efficient, and creative - highly recommended!"',
+    job: 'Marketing Manager'
+  },
+  {
+    name: 'David',
+    img: '/images/avatar-2.png',
+    content: '"Innovative design and seamless execution - exceeded our expectations!"',
+    job: 'CEO'
+  },
+  {
+    name: 'Michelle',
+    img: '/images/avatar-4.png',
+    content: '"Responsive, helpful, and detail-oriented - a pleasure to work with!"',
+    job: 'Project Manager'
+  },
+  {
+    name: 'John',
+    img: '/images/avatar-5.png',
+    content: '"Improved website speed and functionality - thanks for the great work!"',
+    job: 'E-commerce Owner'
+  },
+  {
+    name: 'Samantha',
+    img: '/images/avatar-6.png',
+    content: '"Reliable, knowledgeable, and affordable - I\'ll definitely work with them again!"',
+    job: 'Startup Founder'
+  }
+])
+/**
+ * "Excellent communication and service - transformed our website!" - Alex, Small Business Owner
+"Professional, efficient, and creative - highly recommended!" - Sarah, Marketing Manager
+"Innovative design and seamless execution - exceeded our expectations!" - David, CEO
+"Responsive, helpful, and detail-oriented - a pleasure to work with!" - Michelle, Project Manager
+"Improved website speed and functionality - thanks for the great work!" - John, E-commerce Owner
+"Reliable, knowledgeable, and affordable - I'll definitely work with them again!" - Samantha, Startup Founder
+ */
+
+const { slideIndex } = useSimpleSlide(testimonials.value.length)
+</script>
+
 <template>
   <base-container class="lg:pt-[187px] lg:min-h-[684px] py-10 bg-lavender">
     <section class="lg:flex justify-between gap-10">
@@ -26,7 +77,7 @@
       <article class="relative z-10 lg:mt-0 lg:max-w-[579px] lg:mx-0 mx-auto mt-10 max-w-[600px] text-center">
         <figure class="mb-8">
           <img
-            src="/images/avatar-1.png"
+            :src="testimonials[slideIndex].img"
             alt="avatar"
             class="mx-auto"
           >
@@ -34,22 +85,22 @@
 
         <blockquote>
           <p class="lg:text-[length:23px]">
-            "Excellent communication and service - transformed our website!"
+            {{ testimonials[slideIndex].content }}
           </p>
           <footer class="mt-9">
-            <cite class="lg:text-[length:25px] block not-italic font-bold text-iris text-xl">Alex</cite>
-            <cite class="block not-italic">Small business owner</cite>
+            <cite class="lg:text-[length:25px] block not-italic font-bold text-iris text-xl">{{ testimonials[slideIndex].name }}</cite>
+            <cite class="block not-italic">{{ testimonials[slideIndex].job }}</cite>
           </footer>
         </blockquote>
       </article>
 
       <div class="lg:absolute lg:justify-start lg:mt-0 mt-5 top-[294px] left-0.5 flex justify-center gap-2.5">
-        <span class="w-[17px] h-[17px] rounded-full bg-white"></span>
-        <span class="w-[17px] h-[17px] rounded-full bg-violet"></span>
-        <span class="w-[17px] h-[17px] rounded-full bg-white"></span>
-        <span class="w-[17px] h-[17px] rounded-full bg-white"></span>
-        <span class="w-[17px] h-[17px] rounded-full bg-white"></span>
-        <span class="w-[17px] h-[17px] rounded-full bg-white"></span>
+        <span
+          v-for="(x, index) in testimonials"
+          :key="x"
+          class="w-[17px] h-[17px] rounded-full"
+          :class="index === slideIndex ? 'bg-violet' : 'bg-white'"
+        ></span>
       </div>
     </section>
   </base-container>
