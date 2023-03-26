29e9209104935598013ac8a157fa06328af4ad8e
MRKL\63915 <markluigibatoks@gmail.com>
2023-03-26 18:26:22 +0800

    chore: adjust text style on mobile

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -4,7 +4,7 @@
 <template>
   <base-container class="xl:h-[888px] xl:pt-[229px] xl:pb-0 py-10 ">
     <main class="lg:flex">
-      <div class="lg:order-2 relative w-full">
+      <div class="lg:order-2 lg:mb-0 mb-5 relative w-full">
         <figure class="relative z-10 xl:-top-[229px] lg:top-0 lg:absolute z-10">
           <img
             src="/images/main-img1.png"
@@ -23,13 +23,13 @@
       </div>
 
       <div class="lg:order-1 lg:w-[560px] lg:min-w-[560px] ">
-        <h1 class="lg:text-[length:60px] lg:mb-[30px] leading-normal font-avalon font-bold text-2xl">
+        <h1 class="lg:text-[length:60px] lg:mb-[30px] lg:leading-normal leading-tight font-avalon font-bold text-2xl">
           Hello,
           <span class="lg:-mt-4 block">I’m <em class="lg:whitespace-normal whitespace-nowrap text-accent not-italic">MARK-LUIGI</em> </span>
           <span class="lg:-mt-7 block">Web Developer</span>
         </h1>
 
-        <p class="lg:leading-[40px] lg:text-[23px]">
+        <p class="lg:leading-[40px] lg:text-[23px] lg:text-left text-justify">
           Hi, you can call me Mark, a web developer with x years of experience. I specialize in creating beautiful, responsive, and user-friendly websites, and I offer services that include website conversion, responsive design, website animation, and maintenance. I'm dedicated to delivering high-quality web solutions that help businesses thrive online.
         </p>
       </div>
