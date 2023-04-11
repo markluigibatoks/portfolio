541aa4042bc6f67a3bb23c73f41e24f3f0cd9231
Mark-Luigi Batoctoy <markluigibatoks@gmail.com>
2023-04-11 10:15:57 +0800

    chore: init BaseProjectCard component

---
+++ src/components/BaseProjectCard.vue
@@ -0,0 +1,47 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    default: ''
+  },
+  categories: {
+    type: Array,
+    default: () => []
+  },
+  src: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <div class="relative">
+    <a
+      href="#"
+      target="_blank"
+      class="absolute top-0 right-0"
+    >
+      <icon-code-fill />
+    </a>
+    <figure class="w-20 h-20">
+      <img
+        :src="src"
+        :alt="name"
+      >
+    </figure>
+    <div class="grid grid-cols-12">
+      <div class="col-span-6">
+        {{ name }}
+      </div>
+      <div class="col-span-6">
+        <div
+          v-for="n of categories"
+          :key="n"
+        >
+          {{ n }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
